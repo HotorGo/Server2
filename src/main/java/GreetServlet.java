b84b240d51b25c49744ee845795ecb8918f5cb42
Student <email@example.com@>
2018-04-12 14:27:29 +0300

    1.3

--- src/main/java/GreetServlet.java
+++ src/main/java/GreetServlet.java
@@ -1,23 +1,44 @@
+import freemarker.cache.FileTemplateLoader;
+import freemarker.core.HTMLOutputFormat;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class GreetServlet extends HttpServlet {
+public class GreetServlet extends HttpServlet  {
+    private Configuration conf = new Configuration(  Configuration.VERSION_2_3_28);
+
+
+    public GreetServlet() throws IOException {
+        conf.setDefaultEncoding("UTF-8");
+        conf.setTemplateLoader(new FileTemplateLoader(new File(".")));
+        conf.setOutputFormat(HTMLOutputFormat.INSTANCE);
+    }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         resp.setCharacterEncoding("UTF-8");
-        resp.getWriter().write("<!DOCTYPE html>\n" +
-                "<html lang=\"en\">\n" +
-                "<head>\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title> " + username  + "</title>\n" +
-                "</head>\n" +
-                "<body>\n" +
-                "<h1> Привет  " + username + " !!! </h1>\n<a href=\"/\"> На главную </a>" +
-                "</body>\n" +
-                "</html>");
+        List<String> friends = new ArrayList<>();
+        friends.add("!!!");
+        friends.add("???");
+        Template template = conf.getTemplate("greet.html");
+        Map<String, Object> date = new HashMap<>();
+        date.put("user", username);
+        date.put("friends" , friends);
+        try {
+            template.process(date, resp.getWriter());
+        } catch (TemplateException e) {
+            throw new ServletException(e);
+        }
     }
 }
